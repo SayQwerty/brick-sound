b08324ef58ee67d7cc8eba95a5d0c6bc39a6d43c
Nikita <118116467+SayQwerty@users.noreply.github.com>
2026-09-23 21:39:46 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -31,8 +31,8 @@ window.BS = {
      или готовый embed https://vk.com/video_ext.php?oid=...&id=...
      main.js сам соберёт iframe. */
   vkVideos: [
-    { title: "Видео 1", url: "https://vk.com/video-0000001_456239017" },
-    { title: "Видео 2", url: "https://vk.com/video-0000002_456239018" },
-    { title: "Видео 3", url: "https://vk.com/video-0000003_456239019" }
+    { title: "Аивер – Улыбайлся (акустика)", url: "https://vk.ru/studiobrick?z=clip-17542948_456239740%3Ftracker%3D%257B%2522primaryKey%2522%253A%2522clips_click_top_feed%2522%252C%2522steps%2522%253A%255B%2522modal_open_click%2522%252C%2522modal_init_start%2522%252C%2522modal_first_clip_ready%2522%252C%2522modal_feed_ready%2522%252C%2522modal_init_end%2522%255D%252C%2522mask%2522%253A1%257D" },
+    { title: "KOTOVA – Lost & Found (Lianne La Havas cover)", url: "https://vk.ru/studiobrick?z=video-234146770_456239018%2Fff3042ac61c1d47be5" },
+    { title: "Аивер – акустический лайв", url: "https://vk.ru/studiobrick?z=clip-17542948_456239727%3Ftracker%3D%257B%2522primaryKey%2522%253A%2522clips_click_top_feed%2522%252C%2522steps%2522%253A%255B%2522modal_open_click%2522%252C%2522modal_init_start%2522%252C%2522modal_first_clip_ready%2522%252C%2522modal_feed_ready%2522%252C%2522modal_init_end%2522%255D%252C%2522mask%2522%253A1%257D" }
   ]
 };
